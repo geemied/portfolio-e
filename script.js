4c63f0234768271a678411d165e87e9e68d087ee
jemie <monticalvojeramie@gmail.com>
2025-12-17 09:29:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    if (scrollY >= section.offsetTop - 120) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
